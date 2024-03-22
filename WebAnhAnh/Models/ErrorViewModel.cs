aa7d19b52b44f05ada5135c4ceb1d3b98a5d23f9
nguyenducanhanh <nguyenducanh260402@gmail.com>
2024-03-22 16:22:27 +0700

    LV0

---
+++ WebAnhAnh/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace WebAnhAnh.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
